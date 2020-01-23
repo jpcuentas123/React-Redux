d936d65e748f1205f551dbd691db016e1299d8d5
jpcuentas123 <jpcuentas123@gmail.com>
2020-01-23 11:42:18 -0500

    New State in a single reducer

--- index.js
+++ index.js
@@ -3,18 +3,32 @@ const redux = require('redux');
 const createStore = redux.createStore;
 
 const BUY_CAKE = 'BUY_CAKE';
+const BUY_ICECREAM = 'BUY_ICECREAM';
 
 function buyCake() {
-    return {
-        type: BUY_CAKE,
-        info: 'My first action'
+
+}
+
+const actions = {
+    buyCake: () => {
+        return {
+            type: BUY_CAKE,
+            info: 'Buy cake'
+        }
+    },
+    buyIceCream: () => {
+        return {
+            type: BUY_ICECREAM,
+            info: 'Buy icecream'
+        }
     }
 }
 
 //Reducer = (previousState, action) => State
 
 const initialState = {
-    numOfCakes : 10
+    numOfCakes: 10,
+    numOfIceCream: 20
 }
 
 const reducer = (state = initialState, action) => {
@@ -24,16 +38,22 @@ const reducer = (state = initialState, action) => {
             numOfCakes: state.numOfCakes - 1
         }
             break;
+        case BUY_ICECREAM: return {
+            ...state,
+            numOfIceCream: state.numOfIceCream - 1
+        }
+
+            break;
         default: return state
     }
 }
 
 const store = createStore(reducer);
 
-console.log('Initial state',store.getState());
-const unsubscribe = store.subscribe(() => console.log(`Update state`,store.getState()))
-store.dispatch(buyCake())
-store.dispatch(buyCake())
-store.dispatch(buyCake())
+console.log('Initial state', store.getState());
+const unsubscribe = store.subscribe(() => console.log(`Update state`, store.getState()))
+store.dispatch(actions.buyCake())
+store.dispatch(actions.buyIceCream())
+store.dispatch(actions.buyCake())
 
 unsubscribe();
